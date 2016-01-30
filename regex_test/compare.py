995a5fcdfc8ef333bbbcdb7728c68851a94aca95
David Lathrop <davidlathrop@gmail.com>
2016-01-30 13:39:35 -0500

    More optimizations. Now all in memory instead of writing to disk.

--- regex_test/compare.py
+++ regex_test/compare.py
@@ -1,28 +1,9 @@
 import json
 import os
 
+from common import RegCommon
 
-row_matches = []
-col_matches = []
 grids = []
-
-
-def load_rows():
-    global row_matches
-    pth = os.path.dirname(os.path.abspath(__file__))
-    fp = '{}/rows.txt'.format(pth)
-    with open(fp, 'r') as fd:
-        for line in fd:
-            row_matches.append(frozenset(json.loads(line)))
-
-
-def load_cols():
-    global col_matches
-    pth = os.path.dirname(os.path.abspath(__file__))
-    fp = '{}/cols.txt'.format(pth)
-    with open(fp, 'r') as fd:
-        for line in fd:
-            col_matches.append(json.loads(line))
 
 
 def narrow_letters(groups):
@@ -42,38 +23,34 @@ def reduce_matches(letters, group):
     for idx, items in enumerate(group):
         possible_letters = letters[idx]
         new_group.append([])
-        print("Before: ", len(items))
         for item in items:
             if item[0] not in possible_letters:
                 continue
 
             new_group[idx].append(item)
-        print("After: ", len(new_group[idx]))
 
     return new_group
 
 
 def narrow_choices():
-    global col_matches, row_matches
+    col_letters = narrow_letters(RegCommon.col_matches)
+    RegCommon.row_matches = reduce_matches(col_letters, RegCommon.row_matches)
 
-    col_letters = narrow_letters(col_matches)
-    row_matches = reduce_matches(col_letters, row_matches)
-
-    row_letters = narrow_letters(row_matches)
-    col_matches = reduce_matches(row_letters, col_matches)
+    row_letters = narrow_letters(RegCommon.row_matches)
+    RegCommon.col_matches = reduce_matches(row_letters, RegCommon.col_matches)
     
 
 def compare_cols_to_rows():
-    global col_matches, grids, row_matches
+    global grids
     
-    one, two, three, four = col_matches
+    one, two, three, four = RegCommon.col_matches
       
     for r1 in one:
         for r2 in two:
             for r3 in three:
                 for r4 in four:
                     rows = [[r1[idx],r2[idx],r3[idx],r4[idx]] for idx in range(0,4)]
-                    grid = [''.join(row) in row_matches[idx] for idx, row in enumerate(rows)]
+                    grid = [''.join(row) in RegCommon.row_matches[idx] for idx, row in enumerate(rows)]
                     if all(grid): 
                         grids.append(rows)
 
@@ -89,8 +66,6 @@ def write_grids():
 
 
 def compare_main():
-    load_rows()
-    load_cols()
     narrow_choices()
     compare_cols_to_rows()
     write_grids()
